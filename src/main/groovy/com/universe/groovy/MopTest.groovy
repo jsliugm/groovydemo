27e38cd5e04470eea567538079d3e5bce4af8d3a
liuguangming <jsliugm@gmail.com>
2021-06-27 16:51:05 +0800

    mop

---
+++ src/main/groovy/com/universe/groovy/MopTest.groovy
@@ -0,0 +1,13 @@
+package com.universe.groovy
+
+import com.universe.java.Student
+
+class MopTest{
+    static void main(String[] args) {
+        Student student = new Student()
+        student.setName("abc")
+        Teacher teacher = new Teacher()
+        teacher.setName("wangwu")
+    }
+}
+
---
+++ src/main/groovy/com/universe/groovy/Teacher.groovy
@@ -0,0 +1,22 @@
+package com.universe.groovy
+
+class Teacher{
+    long id;
+    String name;
+
+    long getId() {
+        return id
+    }
+
+    void setId(long id) {
+        this.id = id
+    }
+
+    String getName() {
+        return name
+    }
+
+    void setName(String name) {
+        this.name = name
+    }
+}
